82d7702f4ed2d152e32e7ddb5749c39631728174
Pedro Cotta Badaro <pedrocottabadaro@ice.ufjf.br>
2020-03-05 12:11:33 -0300

    codigo sunil

---
+++ apples/main.py
@@ -0,0 +1,6 @@
+class Apples:
+    def __init__(self,apples):
+        self.apples=apples
+
+    def eat(self,apples):
+        self.apples=self.apples-apples
--- apples/steps/definicaoCenariosApples.py
+++ apples/steps/definicaoCenariosApples.py
@@ -1,39 +1,28 @@
 from behave import *
+from main import Apples
 
 use_step_matcher("re")
 
 
 @given("there are (?P<start>.+) apples")
 def step_impl(context, start):
-    """
-    :type context: behave.runner.Context
-    :type start: str
-    """
-    raise NotImplementedError(u'STEP: Given there are <start> apples')
 
+    #apples=Apples(start)
 
 @when("I eat (?P<eat>.+) apples")
 def step_impl(context, eat):
-    """
-    :type context: behave.runner.Context
-    :type eat: str
-    """
-    raise NotImplementedError(u'STEP: When I eat <eat> apples')
+    #apples.eat(eat)
 
 
 @then("I should have (?P<left>.+) apples")
 def step_impl(context, left):
-    """
-    :type context: behave.runner.Context
-    :type left: str
-    """
-    raise NotImplementedError(u'STEP: Then I should have <left> apples')
+    #print(apples.apples)
+    #print(left)
 
 
-@then('I should have (?P<left>.+) "apples"')
-def step_impl(context, left):
-    """
-    :type context: behave.runner.Context
-    :type left: str
-    """
-    raise NotImplementedError(u'STEP: Then I should have <left> "apples"')
+def eating():
+    apples=Apples(12)
+    apples.eat(5)
+
+
+####PAGINA 30 SUNIL
